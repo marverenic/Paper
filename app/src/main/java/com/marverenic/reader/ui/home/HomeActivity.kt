4a960c10ac68b9cf5939a5eea962eb849967c226
Andrew Bailey <marverenic@gmail.com>
2017-08-23 10:58:40 -0400

    Show navigation drawer on home page

--- app/src/main/java/com/marverenic/reader/ui/home/HomeActivity.kt
+++ app/src/main/java/com/marverenic/reader/ui/home/HomeActivity.kt
@@ -3,7 +3,10 @@ package com.marverenic.reader.ui.home
 import android.content.Context
 import android.content.Intent
 import android.databinding.DataBindingUtil
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
 import com.marverenic.reader.R
 import com.marverenic.reader.databinding.ActivityHomeBinding
 import com.marverenic.reader.ui.BaseActivity
@@ -20,12 +23,26 @@ class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        val content = binding.homeContent!!
 
         supportFragmentManager.beginTransaction()
-                .add(binding.homeFragmentContainer.id, CategoriesFragment.newInstance())
+                .add(content.homeFragmentContainer.id, CategoriesFragment.newInstance())
                 .commit()
 
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(content.toolbar)
+        supportActionBar?.let {
+            it.setHomeAsUpIndicator(R.drawable.ic_menu_24dp)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        binding.homeDrawerLayout.openDrawer(GravityCompat.START)
+        return true
     }
 
 }
