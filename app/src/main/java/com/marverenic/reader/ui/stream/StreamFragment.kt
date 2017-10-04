ef01ef3f5c0b2f4d82ca93b0d37f6bb27108358f
Andrew Bailey <marverenic@gmail.com>
2017-10-04 16:54:16 -0400

    Show "unread only" filter button on streams

--- app/src/main/java/com/marverenic/reader/ui/stream/StreamFragment.kt
+++ app/src/main/java/com/marverenic/reader/ui/stream/StreamFragment.kt
@@ -3,20 +3,21 @@ package com.marverenic.reader.ui.stream
 import android.annotation.SuppressLint
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.marverenic.reader.R
 import com.marverenic.reader.ReaderApplication
 import com.marverenic.reader.data.RssStore
 import com.marverenic.reader.databinding.FragmentStreamBinding
 import com.marverenic.reader.ui.ToolbarFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 private const val ARG_STREAM_NAME = "stream_name"
 private const val ARG_STREAM_ID = "stream_id"
+
+private const val EXTRA_UNREAD_ONLY = "unread_only"
 
 class StreamFragment : ToolbarFragment() {
 
@@ -32,6 +33,10 @@ class StreamFragment : ToolbarFragment() {
     private val streamId: String
         get() = arguments.getString(ARG_STREAM_ID)
 
+    private var unreadOnly: Boolean = false
+
+    private var streamSubscription: Disposable? = null
+
     companion object {
         @SuppressLint("NewApi")
         fun newInstance(streamName: String, streamId: String) = StreamFragment().apply {
@@ -44,7 +49,17 @@ class StreamFragment : ToolbarFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        savedInstanceState?.let {
+            unreadOnly = it.getBoolean(EXTRA_UNREAD_ONLY, false)
+        }
+
         ReaderApplication.component(this).inject(this)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(EXTRA_UNREAD_ONLY, unreadOnly)
     }
 
     override fun onCreateContentView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
@@ -59,8 +74,25 @@ class StreamFragment : ToolbarFragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.article_list, menu)
+        menu.findItem(R.id.menu_item_show_all).isVisible = unreadOnly
+        menu.findItem(R.id.menu_item_filter_unread).isVisible = !unreadOnly
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_show_all -> filterUnreadArticles(false)
+            R.id.menu_item_filter_unread -> filterUnreadArticles(true)
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
     private fun loadArticles(viewModel: StreamViewModel) {
-        rssStore.getStream(streamId)
+        streamSubscription?.dispose()
+        streamSubscription = rssStore.getStream(streamId, unreadOnly)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .compose(bindToLifecycle())
@@ -79,6 +111,14 @@ class StreamFragment : ToolbarFragment() {
                         rssStore.refreshStream(streamId)
                     }
                 })
+    }
+
+    private fun filterUnreadArticles(hideReadArticles: Boolean) {
+        if (unreadOnly != hideReadArticles) {
+            unreadOnly = hideReadArticles
+            loadArticles(binding.viewModel ?: throw IllegalStateException("View model is not set"))
+            activity.invalidateOptionsMenu()
+        }
     }
 
 }
