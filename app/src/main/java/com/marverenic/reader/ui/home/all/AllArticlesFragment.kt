2b85eb57e15da596abc6ff0e37da3d47a78eaafa
Andrew Bailey <marverenic@gmail.com>
2017-10-04 17:07:31 -0400

    Allow read articles to be filtered out on the home screen

--- app/src/main/java/com/marverenic/reader/ui/home/all/AllArticlesFragment.kt
+++ app/src/main/java/com/marverenic/reader/ui/home/all/AllArticlesFragment.kt
@@ -2,17 +2,18 @@ package com.marverenic.reader.ui.home.all
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.marverenic.reader.R
 import com.marverenic.reader.ReaderApplication
 import com.marverenic.reader.data.RssStore
 import com.marverenic.reader.databinding.FragmentAllArticlesBinding
 import com.marverenic.reader.ui.home.HomeFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
+
+private const val EXTRA_UNREAD_ONLY = "unread_only"
 
 class AllArticlesFragment : HomeFragment() {
 
@@ -23,13 +24,27 @@ class AllArticlesFragment : HomeFragment() {
     override val title: String
         get() = getString(R.string.all_articles_header)
 
+    private var unreadOnly: Boolean = false
+
+    private var streamSubscription: Disposable? = null
+
     companion object {
         fun newInstance() = AllArticlesFragment()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        savedInstanceState?.let {
+            unreadOnly = it.getBoolean(EXTRA_UNREAD_ONLY, false)
+        }
+
         ReaderApplication.component(this).inject(this)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(EXTRA_UNREAD_ONLY, unreadOnly)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -44,8 +59,25 @@ class AllArticlesFragment : HomeFragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.article_list, menu)
+        menu.findItem(R.id.menu_item_show_all).isVisible = unreadOnly
+        menu.findItem(R.id.menu_item_filter_unread).isVisible = !unreadOnly
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_item_show_all -> filterUnreadArticles(false)
+            R.id.menu_item_filter_unread -> filterUnreadArticles(true)
+            else -> return super.onOptionsItemSelected(item)
+        }
+        return true
+    }
+
     private fun loadArticles(viewModel: AllArticlesViewModel) {
-        rssStore.getAllArticles()
+        streamSubscription?.dispose()
+        streamSubscription = rssStore.getAllArticles(unreadOnly)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .compose(bindToLifecycle())
@@ -64,6 +96,15 @@ class AllArticlesFragment : HomeFragment() {
                         rssStore.refreshAllArticles()
                     }
                 })
+    }
+
+
+    private fun filterUnreadArticles(hideReadArticles: Boolean) {
+        if (unreadOnly != hideReadArticles) {
+            unreadOnly = hideReadArticles
+            loadArticles(binding.viewModel ?: throw IllegalStateException("View model is not set"))
+            activity.invalidateOptionsMenu()
+        }
     }
 
 }
