ad824f0fead0d44ed6f4ba519aa8270fdad5c8d0
Andrew Bailey <marverenic@gmail.com>
2017-10-05 13:13:01 -0400

    Fix listener not being released

--- app/src/main/java/com/marverenic/reader/utils/ViewUtils.kt
+++ app/src/main/java/com/marverenic/reader/utils/ViewUtils.kt
@@ -1,13 +1,17 @@
 package com.marverenic.reader.utils
 
 import android.view.View
+import android.view.ViewTreeObserver
 
 inline fun <V : View> V.whenMeasured(crossinline action: V.() -> Unit) {
     if (isLaidOut) {
         action()
     } else {
-        viewTreeObserver.addOnGlobalLayoutListener {
-            action()
-        }
+        viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                action()
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        })
     }
 }
